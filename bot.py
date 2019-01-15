0e1a3db32ec487a810f32ef0fde3894a2f9e7c56
seanemac <S.mccann11@nuigalway.ie>
2019-01-15 22:24:34 +0000

    Chuck norris works

--- bot.py
+++ bot.py
@@ -1,25 +1,21 @@
 import os
 import time
 import re
+import requests
 from slackclient import SlackClient
 
-
-# instantiate Slack client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-# starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
-# constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "do"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+AVAIL_COMMANDS = "joke, time or fortune" # can add more
 
+'''
+    These 2 functions determine if a message is directed at the bot
+    If it is, the handle_command function is called
+'''
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
@@ -28,34 +24,47 @@ def parse_bot_commands(slack_events):
     return None, None
 
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
     matches = re.search(MENTION_REGEX, message_text)
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
+'''
+    This function checks if a command exists
+    If it does, it performs the command
+    This is where Code will go
+'''
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
-
-    # Finds and executes the given command, filling in response
+    default_response = "Not sure what you mean. Try *{}*.".format(AVAIL_COMMANDS)
     response = None
-    # This is where you start to implement more commands!
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
 
-    # Sends the response back to the channel
+    if command.startswith("joke"):
+        response = get_joke()
+
+    elif command.startswith("time"):
+        response = "Time code will go here"
+
+    elif command.startswith("fortune"):
+        response = "fortune code will go here"
+
+    # Send response back to slack channel
     slack_client.api_call(
         "chat.postMessage",
         channel=channel,
         text=response or default_response
     )
 
+'''
+    Send request to the Internet Chuck Norris DataBase
+    Get a random joke
+'''
+def get_joke():
+    res = requests.get("http://api.icndb.com/jokes/random")
+    data = res.json()
+    return data["value"]["joke"]
+
+'''
+    Runs the bot, checks messages every second
+'''
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
         print("Starter Bot connected and running!")
